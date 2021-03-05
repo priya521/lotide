c6f9cb1d393551d4d7008687f6e2ce173e239224
priya521 <priya521.j@gmail.com>
2021-03-05 02:08:11 +0000

    adding  findKey.js file

---
+++ findKey.js
@@ -0,0 +1,38 @@
+
+const findKey = function (object, callback) {
+  for (const key in object) {
+    // console.log(object[key])
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+  //   // //if (callback returns true on object.key){
+  //   //   return true;
+  //   //   break;
+}
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+  return;
+};
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+}, x => x.stars === 2), "noma")
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+}, x => x.stars === 3), "noma")
