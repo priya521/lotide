723051c4b7c43e1019d0c9b38c390f41c75d160c
priya521 <priya521.j@gmail.com>
2021-03-08 22:33:16 +0000

    adding  assertArraysEqualTest.js to lotide

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,16 @@
+const assertArraysEqual = require('../assertArraysEqual');
+
+
+
+// function assertArraysEqual(a1,a2){
+//   if(eqArrays(a1,a2)){
+//     console.log(`✅✅✅Assertion Passed:${a1}===${a2}`);
+//   } else {
+//     console.log(`🛑🛑🛑Assertion Failed:${a1}!=${a2}`);
+//   }
+
+// }
+assertArraysEqual([1,2,3],[1,2,3]);
+assertArraysEqual(["1","2"],[1,2]);
+
+
