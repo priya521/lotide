2f5d96ed27444bd4fd4d1beff0ba7ec6800def8a
priya521 <priya521.j@gmail.com>
2021-03-09 03:27:11 +0000

    adding index file

---
+++ index.js
@@ -0,0 +1,38 @@
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const countletters = require('./countletters');
+const countOnly = require('./countOnly');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const letterPositions = require('./letterPositions')
+const map = require('./map')
+const takeUntil = require('./takeUntil')
+const without = require('./without')
+
+module.exports = {
+  head:   head,
+  tail:   tail,
+  middle: middle,
+  assertArraysEqual :assertArraysEqual,
+  assertEqual :assertEqual,
+  assertObjectsEqual :assertObjectsEqual,
+  countletters : countletters,
+  countOnly :countOnly,
+  eqArrays :eqArrays,
+  eqObjects : eqObjects,
+  findKey :findKey,
+  findKeyByValue : findKeyByValue,
+  letterPositions : letterPositions,
+  map: map,
+  takeUntil: takeUntil,
+  without: without
+
+
+
+};
