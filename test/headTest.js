e70102e56a5505dc7f58edd815b026444f65d20b
priya521 <priya521.j@gmail.com>
2021-03-09 00:37:15 +0000

    adding headTest file

--- test/headTest.js
+++ test/headTest.js
@@ -1,12 +1,29 @@
-const assertEqual = require('../assertEqual');
+// const assertEqual = require('../assertEqual');
+
+// Before Mocha . Using export modules
+// function head(arr){
+//   let first = arr[0];
+//   //console.log(first);
+//   return first;
+// }
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+//
 
 
-function head(arr){
-  let first = arr[0];
-  //console.log(first);
-  return first;
-}
-assertEqual(head([5,6,7]), 5);
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+//After Mocha and chai 
 
+
+const assert = require('chai').assert;
+const head   = require('../head');
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+});
